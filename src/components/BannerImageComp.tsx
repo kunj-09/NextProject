a301bf3896a8e7ff583c6518aca7fb030cd5a5a5
Kunj <kunj.mm@gmail.com>
2024-08-05 21:02:58 +0530

    finallll

--- src/components/BannerImageComp.tsx
+++ src/components/BannerImageComp.tsx
@@ -1,3 +1,5 @@
+// src/components/BannerImageComp.tsx
+
 import React from 'react';
 
 interface BannerImageCompProps {
@@ -10,14 +12,14 @@ interface BannerImageCompProps {
 
 const BannerImageComp: React.FC<BannerImageCompProps> = ({ title, description, image, background, onEdit }) => {
   return (
-    <div className="banner ml-10" style={{ backgroundImage: `url(${background})` }}>
+    <div className="banner" style={{ backgroundImage: `url(${background})` }}>
       <div className="banner-overlay">
         <div className="banner-text">
           <h2>{title}</h2>
           <p>{description}</p>
-          <button className="action-button">Start Now</button>
         </div>
         <img src={image} alt={title} className="banner-image" />
+        <button className="action-button">Start Now</button>
       </div>
       <button className="edit-button" onClick={onEdit}>
         ✏️
